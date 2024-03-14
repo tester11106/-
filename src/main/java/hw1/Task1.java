7d6d58e614d57238146e6706874f5a47f0eec705
tester11106 <sasha.zotov.2004dx@mail.ru>
2024-03-14 20:37:18 +0300

    init commit

--- src/main/java/hw1/Task1.java
+++ src/main/java/hw1/Task1.java
@@ -3,5 +3,7 @@ package hw1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("pisun");
+        System.out.println("pisun");
+        System.out.println("pisun");
     }
 }
