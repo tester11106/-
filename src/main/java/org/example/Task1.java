f318f2e85eb128c15007f75e24815c905d6a6baf
tester11106 <sasha.zotov.2004dx@mail.ru>
2024-03-14 20:25:55 +0300

    init commit

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("iiiiiiiiii");
+        System.out.println("Hellow world");
+    }
+}
