f1907740879570bdcbf5eacfd9e7f0e4165684a8
tester11106 <sasha.zotov.2004dx@mail.ru>
2024-03-14 20:45:18 +0300

    init commit

--- src/main/java/hw1/Task2.java
+++ src/main/java/hw1/Task2.java
@@ -3,6 +3,16 @@ package hw1;
 public class Task2 {
     public static void main(String[] args) {
         System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+        System.out.println("feswerhghtrf");
+
     }
 
 }
