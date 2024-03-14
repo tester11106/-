3ea2f18ba3ffe4664bde00a08d46e22175002eaf
tester11106 <sasha.zotov.2004dx@mail.ru>
2024-03-14 21:05:22 +0300

    init commit

--- src/main/java/hw1/Task2.java
+++ src/main/java/hw1/Task2.java
@@ -3,7 +3,7 @@ package hw1;
 public class Task2 {
     public static void main(String[] args) {
         System.out.println("feswerhghtrf");
-        System.out.println("feswerhghtrf");
+        System.out.println("wrf");
 
 
     }
